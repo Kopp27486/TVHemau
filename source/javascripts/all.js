e3c9b53508cfaef757d118527d668c5a5d0ac4ee
Kopp-Home <c.kopp_89@gmx.de>
2016-03-22 18:16:19 +0100

    Inhalt
    wieder alle inhalte drin

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -4,8 +4,6 @@ $(document).ready(function() {
   var menuToggle = $('#js-centered-navigation-mobile-menu').unbind();
   $('#js-centered-navigation-menu').removeClass("show");
 
-  console.log("asdasd");
-
   menuToggle.on('click', function(e) {
     e.preventDefault();
     $('#js-centered-navigation-menu').slideToggle(function(){
