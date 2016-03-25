beef88da1c960788078df3ff96460de38e468539
Kopp-Home <c.kopp_89@gmx.de>
2016-03-25 15:37:18 +0100

    Style - Layout - Java
    Burger Menü (whuup whuup)

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -13,3 +13,25 @@ $(document).ready(function() {
     });
   });
 });
+
+$('document').ready(function () {
+  var trigger = $('#hamburger'),
+      isClosed = true;
+
+  trigger.click(function () {
+    burgerTime();
+  });
+
+  function burgerTime() {
+    if (isClosed == true) {
+      trigger.removeClass('is-open');
+      trigger.addClass('is-closed');
+      isClosed = false;
+    } else {
+      trigger.removeClass('is-closed');
+      trigger.addClass('is-open');
+      isClosed = true;
+    }
+  }
+
+});
